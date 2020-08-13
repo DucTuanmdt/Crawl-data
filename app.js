6fe1548d5f1dc8abbe7957215105da765ea0c8b1
DucTuanmdt <tuanit381@gmail.com>
2020-08-13 15:50:42 +0700

    [stupid heroku :v] fix to deploy heroku

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
 var bodyParser = require('body-parser')
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
